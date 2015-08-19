84b7be09e29b13d62469c4b96d101fe40167ff57
ke-pan <ke_pan@yeah.net>
2015-08-20 06:37:40 +0800

    new atoi

--- myAtoi.js
+++ myAtoi.js
@@ -4,33 +4,33 @@
  */
 var myAtoi = function(str) {
   var
-    i,
-    find = false,
-    neg = false,
-    num = 0;
-  for (i = 0; i < str.length; i++) {
-    // console.log(str[i], num);
-    if (!find && str[i] !== ' ') {
-      find = true
-      if (str[i] === '-') {
-        neg = true;
-        continue;
-      } else if (str[i] === '+') {
-        neg = false;
-        continue;
-      }
-    }
-    if (find) {
-      if (isNaN(str[i]) || str[i] === ' ') break;
-      num = num * 10 + parseInt(str[i]);
-    }
+    i = 0,
+    sign = 1,
+    num = 0,
+    next = 0;
+  while (i < str.length && str[i] === ' ') {
+    i++;
   }
-  num = neg ? -num : num;
-  if (num > 2147483647) return 2147483647;
-  else if (num < -2147483648) return -2147483648
-  else return num;
+  if (str[i] === '-') {
+    sign = -1;
+    i++;
+  } else if (str[i] === '+') {
+    i++;
+  }
+  // console.log(i, str[i]);
+  while (!isNaN(str[i]) && str[i] !== ' ') {
+    // console.log(i, str[i])
+    next = parseInt(str[i]);
+    if (num > 214748364 || num == 214748364 && next >= 8) {
+      return sign < 0 ? -2147483648 : 2147483647;
+    }
+    num = num * 10 + next;
+    i++;
+  }
+  // console.log(num);
+  return sign * num;
 };
 
 console.assert(myAtoi('  ') === 0);
-console.assert(myAtoi(' -1 ') === -1);
+console.assert(myAtoi(' -1') === -1);
 console.assert(myAtoi(' 12k23 ') === 12);
