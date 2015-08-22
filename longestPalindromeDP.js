45cd1365d6e7a571ded784fec826c2373e64331f
ke-pan <ke_pan@yeah.net>
2015-08-22 18:11:45 +0800

    longest palindrome using DP

---
+++ longestPalindromeDP.js
@@ -0,0 +1,37 @@
+/**
+ *Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  var palMatrix = [];
+  var i, j, maxLength = 0;
+  var result = '';
+
+  for (i = 0; i < s.length; i++) {
+    palMatrix[i] = [];
+  }
+
+  for (i = 0; i < s.length; i++) {
+    for (j = 0; j <= i; j++) {
+      if (i == j) palMatrix[i][j] = true;
+      if (i - j == 1) palMatrix[i][j] = s[i] == s[j];
+      if (i - j > 1) palMatrix[i][j] = palMatrix[i - 1][j + 1] && s[i] == s[j];
+      if (palMatrix[i][j] && maxLength < i + 1 - j) {
+        maxLength = i + 1 - j;
+        result = s.slice(j, i + 1);
+      }
+    }
+  }
+  return result;
+};
+
+console.log(longestPalindrome('abbac'));
+console.log(longestPalindrome('abcddcbe'));
+console.log(longestPalindrome('abcddcbeexfe'));
+console.log(longestPalindrome(
+  "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+));
