416923559ba8d948eed1622e51d1a9257caf5879
ke-pan <ke_pan@yeah.net>
2015-11-15 22:06:01 +0800

    Lowest Common Ancestor of a Binary Search Tree

---
+++ LCA.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(root === null || root == p || root == q) {
+      return root;
+    }
+    
+    var left = lowestCommonAncestor(root.left, p, q)
+    var right = lowestCommonAncestor(root.right, p, q)
+    
+    if (left !== null && right !== null) {
+      return root;
+    }
+    
+    if (left !== null) {
+      return left;
+    }
+    
+    if (right !== null) {
+      return right;
+    }
+    
+    return null;
+};
