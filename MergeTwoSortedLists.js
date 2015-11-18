5f52df549fbbe02ce3175f2039c7a73463aeffd9
ke-pan <ke_pan@yeah.net>
2015-11-19 07:53:24 +0800

    merge two sorted lists

---
+++ MergeTwoSortedLists.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  var node = new ListNode(0);
+  var head = node;
+  
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      node.next = l1;
+      l1 = l1.next;
+    } else {
+      node.next = l2;
+      l2 = l2.next;
+    }
+    node = node.next
+  }
+  
+  if (l1) {
+    node.next = l1;
+  }
+  
+  if (l2) {
+    node.next = l2;
+  }
+  
+  return head.next;
+};
